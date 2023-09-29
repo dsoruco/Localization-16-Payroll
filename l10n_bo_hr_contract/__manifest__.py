4830a28a0e0aedee7cfdb7d0d1801a96cf58700a
mayitov2017 <mayitov2017@gmail.com>
2023-09-29 09:59:30 -0400

    Agregando al contrato información de tipo de medida y motivo de medida

---
+++ l10n_bo_hr_contract/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Datec: Nómina Bolivia",
+    'version': '16.0.0.0.0',
+    'depends': ['l10n_bo_hr'],
+    'author': "Datec",
+    'license': 'OPL-1',
+    'category': 'Payroll Localization',
+    'description': """
+    Bolivia Payroll Localization
+    """,
+    'data': [
+        'data/hr_contract_reason_measurement.xml',
+        'security/ir.model.access.csv',
+        'views/contract_view.xml',
+        'views/contract_reason_measurement_view.xml',
+        # 'report/hr_employee_payroll_report_views.xml',
+
+    ],
+}
