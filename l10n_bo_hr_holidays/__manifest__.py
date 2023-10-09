e7875f61d891e2b09424c482ef74cea1fbb69255
mayitov2017 <mayitov2017@gmail.com>
2023-10-09 00:44:14 -0400

    Cambio de versión

--- l10n_bo_hr_holidays/__manifest__.py
+++ l10n_bo_hr_holidays/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "Datec: Nómina Bolivia",
-    'version': '16.0.0.0.4',
+    'version': '16.0.0.0.5',
     'depends': ['l10n_bo_hr_contract'],
     'author': "Datec",
     'license': 'OPL-1',
