0ae5bd3c67eed0be92a557c32316ca20ed8d39e8
mayitov2017 <mayitov2017@gmail.com>
2023-10-19 05:36:22 -0400

    validando cantidad de caracteres permitidos

--- hr_bo_employee_lastnames/models/hr_employee_base.py
+++ hr_bo_employee_lastnames/models/hr_employee_base.py
@@ -5,8 +5,8 @@ class HrEmployeeBase(models.AbstractModel):
     _inherit = "hr.employee.base"
 
     treatment = fields.Selection([('Sr.', 'Sr.'), ('Sra.', 'Sra.')], string='Tratamiento')
-    firstname = fields.Char("First name")
-    firstname2 = fields.Char("Second name")
-    lastname = fields.Char("Last name")
-    lastname2 = fields.Char("Second last name")
-    married_name = fields.Char("Apellido de casada")
+    firstname = fields.Char("First name", size=20)
+    firstname2 = fields.Char("Second name", size=20)
+    lastname = fields.Char("Last name", size=20)
+    lastname2 = fields.Char("Second last name", size=20)
+    married_name = fields.Char("Apellido de casada", size=20)
