19c4272ec4795272b117deee5629b9e8b5eb8231
mayitov2017 <mayitov2017@gmail.com>
2023-11-16 21:58:12 -0500

    arreglos de redonde y nueva regla NETO imponible

--- l10n_bo_hr_payroll/models/hr_payslip_line.py
+++ l10n_bo_hr_payroll/models/hr_payslip_line.py
@@ -12,7 +12,17 @@ class HrPayslipLine(models.Model):
     def _compute_total(self):
         for line in self:
             line.total = float(line.quantity) * line.amount * line.rate / 100
-            if line.code in ['BASE_IMPONIBLE', 'IMPUESTO', 'TAX_2_SMN', 'IMP_NETO_RC_IVA', 'SALDO_A_FAVOR_FISCO',
-                             'SALDO_A_FAVOR_DEPEND', 'SALDO_A_FAVOR_MES_ANT', 'ACTUALIZACION', 'SAL_ANT_ACT',
-                             'SALDO_UTILIZADO', 'IMP_RET_PAGAR', 'SAL_PROX_MES']:
-                line.total = round(line.total, 0)
+            if line.code in ['GROSS', 'NET', 'TOTAL_AFP', 'TWO_SMN', 'BASE_IMPONIBLE', 'IMPUESTO', 'TAX_2_SMN',
+                             'IMP_NETO_RC_IVA', 'IVA_FORM_110', 'SALDO_A_FAVOR_FISCO', 'SALDO_A_FAVOR_DEPEND',
+                             'SALDO_A_FAVOR_MES_ANT', 'ACTUALIZACION', 'SAL_ANT_ACT', 'SALDO_UTILIZADO',
+                             'IMP_RET_PAGAR', 'SAL_PROX_MES']:
+                line.total = special_round(line.total)
+
+
+
+def special_round(number):
+    parte_decimal = number - int(number)  # Obtener la parte decimal del número
+    if parte_decimal < 0.5:
+        return int(number)
+    else:
+        return int(number) + 1
