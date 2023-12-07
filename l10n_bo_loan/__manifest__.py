d6f0a41b5b49a16da237308c485482982a1474f8
mayitov2017 <mayitov2017@gmail.com>
2023-12-07 18:55:35 -0500

    Agregando modulo para anticipo de sueldo

---
+++ l10n_bo_loan/__manifest__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+#    A part of OpenHRMS Project <https://www.openhrms.com>
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2022-TODAY Cybrosys Technologies (<https://www.cybrosys.com>).
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+###################################################################################
+{
+    'name': 'Open Datec Loan Management',
+    'version': '16.0.1.0.0',
+    'summary': 'Manage Loan Requests',
+    'description': """
+        Le ayuda a gestionar las solicitudes de préstamo del personal de su empresa.
+        """,
+    'category': 'Línea base Bolivia/Human Resources/Payroll',
+    'author': "Datec",
+    'company': 'Datec',
+    'maintainer': 'Datec',
+    'live_test_url': '',
+    'website': "",
+    'depends': [
+        'base', 'l10n_bo_hr_payroll', 'hr', 'account',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/security.xml',
+        'views/hr_loan_seq.xml',
+        'data/salary_rule_loan.xml',
+        'views/hr_loan.xml',
+        'views/hr_payroll.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
