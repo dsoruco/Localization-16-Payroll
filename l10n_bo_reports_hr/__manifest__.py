cb2b428864b3f683ff19d0a2015adeb434c75f05
Guillermo Cordova <gcordova@datec.com.bo>
2024-07-16 12:40:45 -0400

    se realiza la modificacion de boton generar

--- l10n_bo_reports_hr/__manifest__.py
+++ l10n_bo_reports_hr/__manifest__.py
@@ -26,7 +26,7 @@
     'data': [
         'security/ir.model.access.csv',
         'views/res_company.xml',
-        'views/finiquito.xml'
+        'views/finiquito.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- l10n_bo_reports_hr/models/reporte_finiquito.py
+++ l10n_bo_reports_hr/models/reporte_finiquito.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import _,models, fields
 from odoo.exceptions import UserError
 import logging
 import json
@@ -9,33 +9,18 @@ _logger = logging.getLogger(__name__)
 class HrPayrollFiniquito(models.Model):
     _inherit = "hr.payroll.finiquito"
 
-    def finiquito_open_form_action(self):
-        action = {
-            "type": "ir.actions.act_window",
-            "view_mode": "form",
-            "res_model": "reporte.finiquito",
-            "target": "new",
-        }
-
-        return action
-
-
-class ReporteFiniquito(models.TransientModel):
-    _name = "reporte.finiquito"
-    _description = "Formulario para reporte de finiquitos"
-
     doc_type = fields.Selection(
         [
             ("pdf", "PDF"),
             ("csv", "CSV"),
         ],
-        string="Formato de Documento",
+        string="Formato de Documento", default="pdf"
     )
     report_file = fields.Binary("Archivo de reporte", readonly=True)
     file_name = fields.Char("Nombre de archivo")
 
     def get_employee(self):
-        id = self._context.get("active_id")
+        id = self.id
         employee = self.env["hr.payroll.finiquito"].browse(id)
         return employee
 
@@ -47,44 +32,54 @@ class ReporteFiniquito(models.TransientModel):
             "date_end": employee.date_end.strftime("%Y-%m-%d"),
             "months": {
                 "concept": [],
-                "month_total_compensation":round(employee.monthly_compensation_total,2),
-                "seniority_bonus_total":round(employee.seniority_bonus_total,2),
-                "border_bonus_total":round(employee.border_bonus_total,2),
-                "commissions_total":round(employee.commissions_total,2),
-                "overtime_total":round(employee.overtime_total,2),
-                "other_bonuses_total":round(employee.other_bonuses_total,2),
-                "average": round(employee.average,2),
-                "total_months":round(employee.total_total,2)
+                "month_total_compensation": round(
+                    employee.monthly_compensation_total, 2
+                ),
+                "seniority_bonus_total": round(employee.seniority_bonus_total, 2),
+                "border_bonus_total": round(employee.border_bonus_total, 2),
+                "commissions_total": round(employee.commissions_total, 2),
+                "overtime_total": round(employee.overtime_total, 2),
+                "other_bonuses_total": round(employee.other_bonuses_total, 2),
+                "average": round(employee.average, 2),
+                "total_months": round(employee.total_total, 2),
             },
-            "eviction": round(employee.eviction,2),
-            "penalties":round(employee.penalties,2),
-            "indemnity_year":employee.indemnity_year,
-            "indemnity_year_amount":round(employee.indemnity_year_amount,2),
-            "indemnity_month":employee.indemnity_month,
-            "indemnity_month_amount":round(employee.indemnity_month_amount,2),
-            "indemnity_day":employee.indemnity_day,
-            "indemnity_day_amount":round(employee.indemnity_day_amount,2),
-            "christmas_bonus_day":employee.christmas_bonus_day,
-            "christmas_bonus_day_amount":round(employee.christmas_bonus_day_amount,2),
-            "christmas_bonus_month":employee.christmas_bonus_month,
-            "christmas_bonus_month_amount":round(employee.christmas_bonus_month_amount,2),
-            "christmas_bonus_one":employee.christmas_bonus_one,
-            "christmas_bonus_two":employee.christmas_bonus_two,
-            "holidays_days":employee.holidays_days,
-            "holidays_amount":round(employee.holidays_amount,2),
-            "other_extraordinary_bonuses":round(employee.other_extraordinary_bonuses,2),
-            "finiquito":round(employee.finiquito,2)
+            "eviction": round(employee.eviction, 2),
+            "penalties": round(employee.penalties, 2),
+            "indemnity_year": employee.indemnity_year,
+            "indemnity_year_amount": round(employee.indemnity_year_amount, 2),
+            "indemnity_month": employee.indemnity_month,
+            "indemnity_month_amount": round(employee.indemnity_month_amount, 2),
+            "indemnity_day": employee.indemnity_day,
+            "indemnity_day_amount": round(employee.indemnity_day_amount, 2),
+            "christmas_bonus_day": employee.christmas_bonus_day,
+            "christmas_bonus_day_amount": round(employee.christmas_bonus_day_amount, 2),
+            "christmas_bonus_month": employee.christmas_bonus_month,
+            "christmas_bonus_month_amount": round(
+                employee.christmas_bonus_month_amount, 2
+            ),
+            "christmas_bonus_one": employee.christmas_bonus_one,
+            "christmas_bonus_two": employee.christmas_bonus_two,
+            "holidays_days": employee.holidays_days,
+            "holidays_amount": round(employee.holidays_amount, 2),
+            "other_extraordinary_bonuses": round(
+                employee.other_extraordinary_bonuses, 2
+            ),
+            "finiquito": round(employee.finiquito, 2),
         }
 
         for i in range(1, 4):
             month_data = {
-                f"compensation{i}": round(getattr(employee, f"monthly_compensation{i}"),2),
-                f"seniority_bonus{i}": round(getattr(employee, f"seniority_bonus{i}"),2),
-                f"border_bonus{i}": round(getattr(employee, f"border_bonus{i}"),2),
-                f"commissions{i}": round(getattr(employee, f"commissions{i}"),2),
-                f"overtime{i}": round(getattr(employee, f"overtime{i}"),2),
-                f"other_bonuses{i}": round(getattr(employee, f"other_bonuses{i}"),2),
-                f"total_month{i}": round(getattr(employee, f"total{i}"),2),
+                f"compensation{i}": round(
+                    getattr(employee, f"monthly_compensation{i}"), 2
+                ),
+                f"seniority_bonus{i}": round(
+                    getattr(employee, f"seniority_bonus{i}"), 2
+                ),
+                f"border_bonus{i}": round(getattr(employee, f"border_bonus{i}"), 2),
+                f"commissions{i}": round(getattr(employee, f"commissions{i}"), 2),
+                f"overtime{i}": round(getattr(employee, f"overtime{i}"), 2),
+                f"other_bonuses{i}": round(getattr(employee, f"other_bonuses{i}"), 2),
+                f"total_month{i}": round(getattr(employee, f"total{i}"), 2),
             }
             data["months"]["concept"].append(month_data)
 
@@ -99,5 +94,43 @@ class ReporteFiniquito(models.TransientModel):
         api_key = company.report_api_key
         url_service = company.url_report_service
         data = self.generate_data(employee)
+        # TODO: hacer conexión con servicio
+        resp = False
 
-        return True
+        if resp:
+            self.file_name = 'report.pdf'
+            return {
+                'type': 'ir.actions.act_url',
+                'url': f'/web/content?model={self.employee_id.display_name}&id={self.id}&field=report_file&filename_field=file_name&download=true',
+                'target': 'new',
+            }
+        else:
+            notification = {
+                "type": "ir.actions.client",
+                "tag": "display_notification",
+                "params": {
+                    "title": _("Advertencia"),
+                    "type": "warning",
+                    "message": "Error al realizar petición, por favor intenta de nuevo!",
+                    "sticky": True,
+                },
+            }
+            return notification
+        
+    # action para abrir popup en caso de necesitarse
+    def finiquito_open_form_action(self):
+        action = {
+            "type": "ir.actions.act_window",
+            "view_mode": "form",
+            "res_model": "reporte.finiquito",
+            "target": "new",
+        }
+
+        return action
+
+
+# popup a generarse para reporte csv y pdf
+# class ReporteFiniquito(models.TransientModel):
+#     _name = "reporte.finiquito"
+#     _description = "Formulario para reporte de finiquitos"
+
