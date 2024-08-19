f63dab73baba8c58b5804dd1d1596038663719ae
Guillermo Cordova <gcordova@datec.com.bo>
2024-08-19 17:20:57 -0400

    se realiza vista para rpi corregida y se realiza creacion de json

--- l10n_bo_reports_hr/models/reporte_impositiva.py
+++ l10n_bo_reports_hr/models/reporte_impositiva.py
@@ -1,17 +1,15 @@
 from odoo import models, fields
+import json
 
 
 class HrPayrollRpi(models.Model):
     _name = "hr.payroll.rpi"
     _description = "Reporte de Planilla Impositiva"
 
-    doc_type = fields.Selection(
-        [
-            ("pdf", "PDF"),
-            ("csv", "CSV"),
-        ],
-        string="Formato de Documento",
-        default="pdf",
+    form_type = fields.Selection(
+        [("planilla", "Planilla Impositiva"), ("form608", "Formulario 608")],
+        string="Tipo de Reporte",
+        default="planilla",
     )
     month = fields.Selection(
         [
@@ -35,5 +33,60 @@ class HrPayrollRpi(models.Model):
     report_file = fields.Binary("Archivo de reporte", readonly=True)
     file_name = fields.Char("Nombre de archivo")
 
+    def get_employee_ids(self):
+        employees = self.env["hr.payslip"].search([("state", "=", "paid")])
+        data = []
+        for pay in employees:
+            if pay.date_from.year == int(self.year) and pay.date_from.month == int(
+                self.month
+            ):
+                data.append(pay)
+        return data
+
+    def get_rpi_data(self, index, employee):
+        data = {
+            "nro": index,
+            "year": self.year,
+            "month": self.month,
+            "tax_code":"",
+            "name":"",
+            "last_name1":"",
+            "last_name2":"",
+            "document_value":"",
+            "document_type":"",
+            "employee_status":"",
+            "contract_wage":employee.contract_id.contract_wage,
+            "tax_free":"",
+            "tax_base":"",
+            "tax_iva":"",
+            "tax_2smn":"",
+            "net_tax":"",
+            "f110":"",
+            "tax_to_pay":"",
+            "dependent_balance":"",
+            "last_dependent_balance":"20",
+            "last_ufv_dependent_balance":"",
+            "updated_dependent_balance":"",
+            "balance_used":"",
+            "withheld_tax":"",
+            "updated_balance_credit":""
+            
+        }
+        return data
+
+    def generate_data(self):
+        employee_ids = self.get_employee_ids()
+        index = 1
+        employee_data = []
+
+        for employee in employee_ids:
+            employee_data.append(self.get_rpi_data(index, employee))
+            index += 1
+        return json.dumps(
+            {"report": f'rpi_{self.form_type}', "extension": "csv", "data": employee_data}
+        )
+
     def action_generate_report(self):
+        data = self.generate_data()
+        self.file_name = f"PLA_NIT_{self.year}_{self.month}"
         return True
